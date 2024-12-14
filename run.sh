f81f8085f2e63dd248a3e3a09c9621a6d8fd0476
Scrrrr <115068570+Scrrrr@users.noreply.github.com>
2024-12-15 00:58:01 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+packages = (
+  "task-japanese"
+  "task-japanese-desktop"
+  "fcitx5-mozc"
+  "cargo"
+  "gobuster"
+  "z"
+  "fzf"
+)
+
 #fcitx5 Theme
 git clone https://github.com/waleslau/fcitx5-themes.git /tmp/fcitx5-themes
 mkdir -p ~/.local/share/fcitx5/themes
@@ -9,10 +19,12 @@ mv /tmp/fcitx5-themes/Alpha-black ~/.local/share/fcitx5/themes
 dpkg-reconfigure locales
 update-locale LANG=ja_JP.UTF-8
 
-packages = (
-  "task-japanese"
-  "task-japanese-desktop"
-  "fcitx5-mozc"
-  "cargo"
-  "gobuster"
-)
+#install z command
+git clone https://github.com/rupa/z.git ~/.z
+echo ". ~/.z/z.sh" >> ~/.bash_profile
+
+#setting fzf 
+echo "source /usr/share/doc/fzf/examples/key-bindings.zsh" >> ~/.zshrc
+echo "source /usr/share/doc/fzf/examples/completion.zsh" >> ~/.zshrc
+
+
